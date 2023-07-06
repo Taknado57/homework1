d2643f76bcd804741c16f202dceeefb7f4aae504
Urmatbek <urmat0791@bk.ru>
2023-07-06 14:00:31 +0600

    домашная задания №1

--- dom01/Program.cs
+++ dom01/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+Console.WriteLine("Введите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if(firstNumber > secondNumber)
+{
+   Console.WriteLine("max = firstNumber");
+   Console.WriteLine("min = secondNumber");
+}
+else
+{
+    Console.WriteLine("max = secondNumber");
+   Console.WriteLine("min = firstNumber");
+}
