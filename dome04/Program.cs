044abd89ac335d2036db322d72c92254154beed2
Urmatbek <urmat0791@bk.ru>
2023-07-07 19:29:08 +0600

    домашнее задание

--- dome04/Program.cs
+++ dome04/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N),
+//  а на выходе показывает все чётные числа от 1 до N
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Number = 2;
+
+if(number > 1)
+{
+    while(Number <= number)
+    {
+        Console.Write(Number + " ");
+        Number = Number + 2;
+    }
+}
