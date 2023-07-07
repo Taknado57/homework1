b9ac3b970e1f1566bd01b71eca87efe4397293dd
Urmatbek <urmat0791@bk.ru>
2023-07-07 11:21:51 +0600

    домашнее задание 3

--- dom03/Program.cs
+++ dom03/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+if (int.Parse(Console.ReadLine()) % 2 == 0)
+{
+    Console.WriteLine("Это чётное число");
+}           
+else 
+{
+    Console.WriteLine("Это нечётное число");
+}
